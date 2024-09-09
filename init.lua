edc1cce937a4c67d0635b59161ab12b9ca8988eb
fruit salad <124645742+fruitsaladchan@users.noreply.github.com>
2024-09-09 16:57:37 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,20 +1,159 @@
-local function noremap(mode, lhs, rhs, opts)
-  local options = { noremap = true, silent = true }
-  if opts then
-    options = vim.tbl_extend("force", options, opts)
-  end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-noremap('n', 'W', 'b')
-noremap('n', ';', ':')
-noremap('n', 'x', '"_x')
-noremap('n', 'c', '"_c')
-noremap('n', 'd', '"_d')
-noremap('v', 'd', '"_d')
-noremap('v', 'c', '"_c')
+require("lazy").setup({
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
-vim.api.nvim_set_keymap('n', 'S', ':%s//g<left><left>', {noremap = true, silent = true})
+  {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
 
-vim.o.relativenumber = true
-undofile = true
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("lspconfig").bashls.setup{}
+
+      require("lspconfig").pyright.setup{}
+    end,
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip", -- Snippet engine
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = {
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+          ["<C-n>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-p>"] = cmp.mapping.scroll_docs(4),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        },
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+        },
+      })
+    end,
+  },
+
+  {
+    "kyazdani42/nvim-tree.lua",
+    dependencies = { "kyazdani42/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup()
+    end,
+  },
+
+  {
+    "tpope/vim-fugitive",
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          mappings = {
+            i = {
+              ["<C-n>"] = require("telescope.actions").move_selection_next,
+              ["<C-p>"] = require("telescope.actions").move_selection_previous,
+            },
+          },
+        },
+      })
+    end,
+  },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "kyazdani42/nvim-web-devicons", opt = true },
+    config = function()
+      require("lualine").setup({
+        options = { theme = "catppuccin" },
+      })
+    end,
+  },
+
+  {
+    "tpope/vim-surround",
+  },
+
+  {
+    "tpope/vim-commentary",
+  },
+
+  {
+    "junegunn/vim-easy-align",
+  },
+
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      require("ibl").setup({
+        scope = {
+          show_start = true,
+          show_end = true,
+        },
+      })
+    end,
+  },
+})
+
+-- General Neovim settings
+vim.opt.number = true              -- Show line numbers
+vim.opt.relativenumber = true      -- Relative line numbers
+vim.opt.tabstop = 4                -- Number of spaces tabs count for
+vim.opt.shiftwidth = 4             -- Number of spaces for auto-indents
+vim.opt.expandtab = true           -- Use spaces instead of tabs
+vim.opt.smartcase = true           -- Smart case when searching
+vim.opt.ignorecase = true          -- Ignore case in search
+vim.opt.splitright = true          -- Vertical splits open on the right
+vim.opt.splitbelow = true          -- Horizontal splits open below
+vim.opt.mouse = "a"                -- Enable mouse support
+
+-- Set the Catppuccin colorscheme
+vim.cmd.colorscheme("catppuccin")
+
+-- Keybindings
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true }) -- Toggle file explorer
+vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { noremap = true }) -- Fuzzy find files
+vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>", { noremap = true }) -- Fuzzy grep search
+vim.keymap.set('n', 'W', 'b', { noremap = true })
+vim.keymap.set('n', ';', ':', { noremap = true })
+vim.keymap.set('n', 'x', '"_x', { noremap = true })
+vim.keymap.set('n', 'c', '"_c', { noremap = true })
+vim.keymap.set('n', 'd', '"_d', { noremap = true })
+vim.keymap.set('v', 'd', '"_d', { noremap = true })
+vim.keymap.set('v', 'c', '"_c', { noremap = true })
