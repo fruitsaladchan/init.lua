75172ccc4ac2c190b4063f47880ab92f2d0ad59e
fruit salad <124645742+fruitsaladchan@users.noreply.github.com>
2024-10-05 18:01:01 -0400

    Add files via upload

--- init.lua
+++ init.lua
@@ -16,11 +16,51 @@ require("lazy").setup({
 
 	{
 		"nvim-treesitter/nvim-treesitter",
+		event = { "BufReadPre", "BufNewFile" },
+		build = ":TSUpdate",
 		run = ":TSUpdate",
+		dependencies = {
+			"windwp/nvim-ts-autotag",
+		},
 		config = function()
-			require("nvim-treesitter.configs").setup({
-				highlight = { enable = true },
+			local treesitter = require("nvim-treesitter.configs")
+
+			treesitter.setup({
+				highlight = {
+					enable = true,
+				},
 				indent = { enable = true },
+				autotag = {
+					enable = true,
+				},
+
+				ensure_installed = {
+					"json",
+					"javascript",
+					"typescript",
+					"tsx",
+					"yaml",
+					"html",
+					"css",
+					"graphql",
+					"bash",
+					"lua",
+					"vim",
+					"dockerfile",
+					"gitignore",
+					"gitcommit",
+					"query",
+					"c",
+				},
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<C-space>",
+						node_incremental = "<C-space>",
+						scope_incremental = false,
+						node_decremental = "<bs>",
+					},
+				},
 			})
 		end,
 	},
@@ -112,11 +152,66 @@ require("lazy").setup({
 	},
 	{
 		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			{ "antosha417/nvim-lsp-file-operations", config = true },
+			{ "folke/neodev.nvim", opts = {} },
+		},
 		config = function()
 			local lspconfig = require("lspconfig")
 			local mason_lspconfig = require("mason-lspconfig")
 			local cmp_nvim_lsp = require("cmp_nvim_lsp")
 			local capabilities = cmp_nvim_lsp.default_capabilities()
+
+			local keymap = vim.keymap -- for conciseness
+
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(ev)
+					local opts = { buffer = ev.buf, silent = true }
+
+					-- set keybinds
+					opts.desc = "Show LSP references"
+					keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+
+					opts.desc = "Go to declaration"
+					keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+
+					opts.desc = "Show LSP definitions"
+					keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+
+					opts.desc = "Show LSP implementations"
+					keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+
+					opts.desc = "Show LSP type definitions"
+					keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+
+					opts.desc = "See available code actions"
+					keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+
+					opts.desc = "Smart rename"
+					keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+
+					opts.desc = "Show buffer diagnostics"
+					keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+
+					opts.desc = "Show line diagnostics"
+					keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+
+					opts.desc = "Go to previous diagnostic"
+					keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+
+					opts.desc = "Go to next diagnostic"
+					keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+
+					opts.desc = "Show documentation for what is under cursor"
+					keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+
+					opts.desc = "Restart LSP"
+					keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+				end,
+			})
 
 			local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
 			for type, icon in pairs(signs) do
@@ -124,7 +219,6 @@ require("lazy").setup({
 				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 			end
 			mason_lspconfig.setup_handlers({
-				-- default handler for installed servers
 				function(server_name)
 					lspconfig[server_name].setup({
 						capabilities = capabilities,
@@ -132,8 +226,8 @@ require("lazy").setup({
 				end,
 				["pyright"] = function()
 					lspconfig["pyright"].setup({
-						capabilities = capabilities,
 						on_attach = function() end,
+						capabilities = capabilities,
 					})
 				end,
 				["graphql"] = function()
@@ -146,6 +240,7 @@ require("lazy").setup({
 					lspconfig["bashls"].setup({
 						capabilities = capabilities,
 						on_attach = function() end,
+						filetypes = { "sh" },
 					})
 				end,
 				["ast_grep"] = function()
